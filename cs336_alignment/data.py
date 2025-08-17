a0645c530984fc03025f0fe2d5b70d3797de5cc4
Yi Lu <luyi0619@gmail.com>
2025-08-17 08:25:36 +0000

    fix a performance bug

--- cs336_alignment/data.py
+++ cs336_alignment/data.py
@@ -18,27 +18,27 @@ propmt_template = \
 
 class SftDataset(Dataset):
   def __init__(self, tokenizer: PreTrainedTokenizerBase, dataset_path: str | os.PathLike, seq_length: int, shuffle: bool):
-    self.seq_length = seq_length
-
     docs = []
     with open(dataset_path, "r", encoding="utf-8") as f:
       for line in f:
         data = json.loads(line)
         docs.append(propmt_template.format(prompt=data["prompt"], response=data["response"]))
-    
     if shuffle:
       random.shuffle(docs)
 
     text = "<|end_of_text|><|begin_of_text|>".join(docs)
-    self.all_tokens = tokenizer.encode(text)
+    all_tokens = tokenizer.encode(text)
+
+
+    length = (len(all_tokens)-1) // seq_length
+    self.input_ids = torch.tensor(all_tokens[0:seq_length * length], dtype=torch.int32).reshape(length, seq_length)
+    self.labels = torch.tensor(all_tokens[1:seq_length * length+1], dtype=torch.int32).reshape(length, seq_length)
 
   def __len__(self):
-    return (len(self.all_tokens) - 1) // self.seq_length
+    return self.input_ids.shape[0]
   
   def __getitem__(self, i):
-    input_ids = self.all_tokens[i * self.seq_length: (i+1) *  self.seq_length]
-    labels = self.all_tokens[i * self.seq_length + 1: (i+1) *  self.seq_length + 1]
-    return {"input_ids":torch.tensor(input_ids), "labels":torch.tensor(labels)}
+    return {"input_ids":self.input_ids[i], "labels":self.labels[i]}
 
 def iterate_batches(
     dataset: Dataset,
